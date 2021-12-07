def71406c76eca483244686bb01802adc7276500
Rahul2325 <rahul230494@gmail.com>
2021-12-07 15:09:06 +0530

    empty

--- Queue.java
+++ Queue.java
@@ -36,7 +36,7 @@ class Queue {
     
     // Return whether the queue is empty.
     public boolean empty() {
-
+        return (stack1.isEmpty() && stack2.isEmpty()) ? (true): (false);
     }
 
     // Return the number of elements in queue.
